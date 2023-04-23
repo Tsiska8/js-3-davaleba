994d99d539a82a6201eaf1ad7bc60cbf6c5b23dc
Ketevan <qeto71@gmail.com>
2023-04-23 10:53:19 +0400

    update

---
+++ script.js
@@ -0,0 +1,15 @@
+function gamotvla(){
+    var x = document.getElementById(ricxvi).value;
+
+    if (x == 0 ) {
+        window.alert(" არ შეიძლება იყოს 0-ის ტოლი");
+        return;
+    }
+
+
+if( x % 2 == 0) {
+    window.alert("ჩაწერილი რიცხვი არის ლუწი");
+  }else if(x % 2 == 1) {
+    window.alert("ჩაწერილი რიცხვი არის კენტი");
+  }
+}
