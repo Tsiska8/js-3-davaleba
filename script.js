ed332e9be5c1f5f7b06b83a5bb7ef2ab2edaf41c
Ketevan <qeto71@gmail.com>
2023-04-23 21:56:14 +0400

    update

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function gamotvla() {
-    var x = document.getElementById("ricxvi").value;
-    var text = document.getElementById("txt");
-
-    if(x.lengh == 0) {
-      text.innerHTML = "არ შეიძლება იყოს 0-ის ტოლი!";
-      return;
-    }
+    var x = document.getElementById("ricxvi").value;  
 
 if(x < 0) {
     window.alert("რიცხვი უარყოფითია!");
   }else if(x > 0) {
     window.alert("რიცხვი დადებითია!");
+    return;
+  }
+
+  if(x == 0) {
+    window.alert= "არ შეიძლება იყოს 0-ის ტოლი!";
   }
 }
+
